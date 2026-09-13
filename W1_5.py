b812459b5955333c60fce0c005ab33c52a018389
Marika Rahikainen <marika.rahikainen@gmail.com>
2026-09-13 19:04:26 +0300

    testing

--- W1_5.py
+++
@@ -1,5 +0,0 @@
-print("Calculate the area of a wall")
-width = float(input("Enter the width in meters: "))
-height = float(input("Enter the height in meters: "))
-area = width * height
-print(f"The area of the wall is {area} square meters.")
