4da0143041f970390945f2355da3c5374ac32035
Marika Rahikainen <marika.rahikainen@gmail.com>
2026-09-21 11:16:29 +0300

    started w2

---
+++ W2/A2_T1.py
@@ -0,0 +1,8 @@
+print("Program starting.")
+name = input("What is your name?")
+number1 = float(input("Enter a floating point number: "))
+number2 = float(input("Enter a second floating point number: "))
+print(f"{name} you gave numbers {number1} and {number2}.")
+finalfloat = number1 * number2
+print(f"Multiplying {number1} and {number2} will result in  product {round(finalfloat, 2)}.")
+print("Program ending.")
---
+++ W2/A2_T2.py
@@ -0,0 +1,4 @@
+print("Program starting")
+carbrand = input("Insert car brand: ")
+carmodel = input("Insert car model: ")
+print("Car brand is {carbrand, sep = '"'} and the model is {carmodel, end = '''})
