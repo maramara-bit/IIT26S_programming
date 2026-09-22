f8e00d2dc7b9fea880f3b5c93a278046a9739109
Marika Rahikainen <marika.rahikainen@gmail.com>
2026-09-22 20:31:47 +0300

    more w2 task, not finished

---
+++ W2/A2_T6.py
@@ -0,0 +1,13 @@
+print("Program starting.")
+print("\n")
+hexcolor = input("Insert a hex color: ")
+r = hexcolor[1:2:]
+g = hexcolor[3:4:]
+b = hexcolor[5:6:]
+print("\n")
+print("Colors")
+print(f"- Red {r}")
+print(f"- Green {g}")
+print(f"- Blue {b}")
+print("\n")
+print("Program ending.")
---
+++ W2/A2_T7.py
@@ -0,0 +1,4 @@
+print("Program starting.")
+fh = float(input("Insert fahrenheits: "))
+print(f"{fh}F is {(fh-32)/1.8}C")
+print("Program ending.")
